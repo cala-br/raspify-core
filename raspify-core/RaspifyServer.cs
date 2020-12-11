6ea545d52d25aed76852c85b556f20a0a87572db
Stefano Calabretti <StefanoCalabretti@outlook.it>
2020-12-11 01:14:25 +0100

    Change from named pipes to tcp socket

--- raspify-core/RaspifyServer.cs
+++ raspify-core/RaspifyServer.cs
@@ -9,6 +9,8 @@ using ApiExt = RaspifyCore.SpotifyApiExtension;
 using System.IO;
 using System.Collections.Concurrent;
 using System.Threading;
+using System.Net.Sockets;
+using System.Net;
 
 #nullable enable
 
@@ -16,50 +18,75 @@ namespace RaspifyCore
 {
     class RaspifyServer : IDisposable
     {
-        private ConcurrentBag<StreamWriter> _activeServerStreams = new();
+        private ConcurrentDictionary<Socket, Socket> _activeClients = new();
         private CancellationTokenSource _tokenSource = new();
-
 
         public event EventHandler? ClientConnected;
 
 
+        public void Start()
+        {
+            SpawnNew();
+        }
+
+        private async void SpawnNew()
+        {
+            try
+            {
+                await Task.Run(StartNewPipeServer, _tokenSource.Token);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private async Task StartNewPipeServer()
+        {
+            var token = _tokenSource.Token;
+            using var server =
+                new Socket(SocketType.Stream, ProtocolType.Tcp);
+
+            server.Bind(new IPEndPoint(IPAddress.Any, 50_000));
+            server.Listen(1);
+
+            var client = await server.AcceptAsync();
+            token.ThrowIfCancellationRequested();
+
+            ClientConnected?.Invoke(this, new());
+            _activeClients.TryAdd(server, client);
+
+            while (server.Connected)
+                token.ThrowIfCancellationRequested();
+
+            _activeClients.Remove(server, out var _);
+            Console.WriteLine("Done");
+        }
+
+
         public async Task SendAllAsync(string message)
         {
-            foreach(var stream in _activeServerStreams)
+            foreach (var client in _activeClients.Values)
+                await SendOneAsync(client, message);
+        }
+
+
+        private async Task SendOneAsync(Socket client, string message)
+        {
+            try
             {
-                await stream.WriteAsync(message + '\0');
-                await stream.FlushAsync();
+                using var netStream = new NetworkStream(client);
+                using var outStream = new StreamWriter(netStream);
+
+                await outStream.WriteAsync(message + '\0');
+                await outStream.FlushAsync();
+            }
+            catch
+            {
+                Console.WriteLine("Error when sending");
             }
         }
 
-
-        public async void Start()
-        {
-            await Task.Run(StartNewPipeServer, _tokenSource.Token);
-        }
-
-
-        private async void StartNewPipeServer()
-        {
-            using var server =
-                new NamedPipeServerStream("raspify_pipe", PipeDirection.InOut);
-
-            await server.WaitForConnectionAsync(_tokenSource.Token);
-            using var outStream = new StreamWriter(server);
-
-            _tokenSource.Token.ThrowIfCancellationRequested();
-            _activeServerStreams.Add(outStream);
-            ClientConnected?.Invoke(this, null!);
-
-            while (true)
-            {
-                if (_tokenSource.Token.IsCancellationRequested)
-                {
-                    Console.WriteLine("Cancelled");
-                    return;
-                }
-            }
-        }
 
         public void Dispose()
         {
