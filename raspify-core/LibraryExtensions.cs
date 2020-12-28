8a526b92df1ec0e4ca89040963136aeef6a35006
Stefano Calabretti <StefanoCalabretti@outlook.it>
2020-12-28 03:52:12 +0100

    Extend IPEndPoint

---
+++ raspify-core/LibraryExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaspifyCore
+{
+    static class LibraryExtensions
+    {
+        public static EndPoint Clone(this EndPoint source)
+        {
+            return IPEndPoint.Parse(source.ToString());
+        }
+    }
+}
